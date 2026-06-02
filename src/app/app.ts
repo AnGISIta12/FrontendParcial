d239feb861b5c2365a46f4d026c1c9255963dc5e
AnGISIta12 <angylorena0912@gmail.com>
2026-06-02 17:49:08 -0500

    correciones

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BibliotecaFormComponent } from './components/bibliotecaformulario/biblioteca';
+import { BibliotecaTableComponent } from './components/bibliotecatabla/bibliotecatable';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, BibliotecaFormComponent, BibliotecaTableComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('frontend-parcial');
-}
+export class App {}
